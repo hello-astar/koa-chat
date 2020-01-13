993df98e10fb6843dedf87c7b7e15d37a52a3698
astar <2320390964@qq.com>
2020-01-13 15:32:01 +0800

    配置变量

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 // 把下面的这个几个模块安装一下
 // 这只是功能模块完成，后期肯定要连接数据库保存数据
 const Koa = require('koa')
-const koaStatic = require('koa-static')
+const koaStatic = require('koa-static') // 处理静态资源
 const path = require('path')
 // 路由
 const route = require('koa-route')
 // koa封装的websocket这是官网（很简单有时间去看一下https://www.npmjs.com/package/koa-websocket）
 const websockify = require('koa-websocket')
 const app = websockify(new Koa());
-
+const config = require('./config')
 // 托管静态文件
 app.use(koaStatic(path.resolve(__dirname, 'static')))
 
@@ -22,7 +22,10 @@ app.ws.use(route.all('/', function (ctx) {
         ctx.websocket.send(message)
     })
 }))
-app.listen(3000, () => {
-  console.log('http://127.0.0.1:3000')
+app.on('error', (err, ctx) =>
+  console.error('server error', err)
+)
+app.listen(config.PORT, () => {
+  console.log(`${config.BASE_URL}:${config.PORT}`)
 })
 // 会默认打开127.0.0.1:3000这个端口号
--- config/index.js
+++ config/index.js
@@ -1,1 +1,2 @@
-exports.PORT = process.env.PORT || 3000;
+exports.PORT = 3000;
+exports.BASE_URL = 'http://192.168.20.59'
