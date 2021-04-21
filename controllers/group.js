290e93e83c99e99b5210be3bf985e6eaea2ea8a2
astar <2320390964@qq.com>
2021-04-22 00:19:35 +0800

    fix: merge group avatar

--- controllers/group.js
+++ controllers/group.js
@@ -2,7 +2,7 @@
  * @Author: astar
  * @Date: 2021-04-19 13:54:52
  * @LastEditors: astar
- * @LastEditTime: 2021-04-21 18:57:02
+ * @LastEditTime: 2021-04-22 00:19:07
  * @Description: 文件描述
  * @FilePath: \koa-chat\controllers\group.js
  */
@@ -43,8 +43,9 @@ class ChatController {
                           requests.push(avatarBuffer)
                         });
                         let avatarBuffers = (await axios.all(requests)).map(item => item.data);
-                        let size = 500;
-                        let eachSize = Math.floor(Math.sqrt(size * size / avatarBuffers.length));
+                        let size = 150;
+                        let eachSize = 50;
+                        // let eachSize = Math.floor(Math.sqrt(size * size / avatarBuffers.length));
                         let columns = Math.floor(size / eachSize);
                         let x = 0, y = 0;
                         // 获取背景
@@ -53,11 +54,11 @@ class ChatController {
                             width: size,
                             height: size,
                             channels: 4,
-                            background: { r: 0, g: 0, b: 0, alpha: 128 }
+                            background: { r: 255, g: 255, b: 255, alpha: 128 }
                           }
                         }).raw().toBuffer();
 
-                        const composite = await (avatarBuffers.reduce((input, overlay) => {
+                        let composite = await (avatarBuffers.reduce((input, overlay) => {
                           return input.then(async function (data) {
                             let temp = sharp(data, { raw: { width: size, height: size, channels: 4 } })
                                         .composite([{
@@ -76,12 +77,17 @@ class ChatController {
                           })
                         }, backgroundBuffer));
 
-                        let last = await sharp(composite).png().toBuffer()
+                        composite = await sharp(composite, { raw: {
+                          width: size,
+                          height: size,
+                          channels: 4
+                        }}).png().toBuffer();
+
                         return {
                           _id: item._id,
                           groupName: item.groupName,
                           addTime: item.addTime,
-                          avatar: 'data:image/png;base64,'+ last.toString('base64')
+                          avatar: 'data:image/png;base64,'+ composite.toString('base64')
                         }
                       }));
                     });
