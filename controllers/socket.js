a4e7783e7c98fd3f029977a077b6b253411edbe6
astar <2320390964@qq.com>
2021-05-08 17:32:40 +0800

    feat: add socket controller

---
+++ controllers/socket.js
@@ -0,0 +1,73 @@
+/*
+ * @Author: astar
+ * @Date: 2021-05-08 16:36:51
+ * @LastEditors: astar
+ * @LastEditTime: 2021-05-08 17:30:06
+ * @Description: 文件描述
+ * @FilePath: \koa-chat\controllers\socket.js
+ */
+const getModel = require('@models').getModel;
+const groupModel = getModel('groupmodel');
+const chatModel = getModel('chatmodel');
+const userModel = getModel('usermodel');
+
+const socket = {};
+
+socket.handleSocketConnect = async (io, socket) => {
+  const index = io.onlineList.findIndex(item => item.decoded_token._id === socket.decoded_token._id);
+  if (index !== -1) {
+    io.onlineList[index].emit('error', { type: 'logout', msg: '该用户已在别处登录' });
+    io.onlineList.splice(index, 1);
+  };
+  io.onlineList.push(socket);
+  console.log(`${socket.decoded_token.userName}连接`);
+}
+
+socket.handleReceiveMessage = async (io, socket, msg) => {
+  const { _id, lastOnlineTime } = socket.decoded_token;
+  const userInfo = await userModel.findOne({ _id });
+  if (!userInfo) return socket.$emit('error', { type: 'logout', msg: '未授权，请重新登录' });
+  if(new Date(lastOnlineTime).getTime() !== new Date(userInfo.lastOnlineTime).getTime()) {
+    return socket.$emit('error', { type: 'logout', msg: '未授权，请重新登录' });
+  }
+
+  try {
+    const chat = {
+      sender: _id,
+      content: msg.content,
+      receiver: msg.receiverId,
+      receiverModel: msg.isGroup ? 'groupmodel' : 'usermodel'
+    };
+    let chatInfo = await chatModel.create(chat);
+    chatInfo = await chatModel.findOne({ _id: chatInfo._id })
+                              .populate('sender', ['userName', 'avatar'])
+                              .populate('receiver', ['groupName', 'userName', 'avatar']);
+    // 若发送给群
+    // 拥有这个群的所有在线用户都要收到消息
+    if (msg.isGroup) {
+      let group = await groupModel.findOne({ _id: msg.receiverId, members: { $in: io.onlineList.map(item => item.decoded_token._id) } }, { members: 1 });
+      if (group) {
+        group.members.forEach(item => {
+          let online = io.onlineList.find(ele => String(ele.decoded_token._id) === String(item));
+          online && online.emit('message', chatInfo);
+        });
+      }
+    } else { // 发送给个人
+      let online = io.onlineList.find(item => String(item.decoded_token._id) === String(msg.receiverId));
+      online && online.emit('message', chatInfo);
+    }
+  } catch (e) {
+    console.log(e)
+    socket.emit('error', { type: 'message', msg: e });
+  }
+}
+
+socket.handleSocketDisconnect = (io, socket, reason) => {
+  const index = io.onlineList.findIndex(item => item.decoded_token._id === socket.decoded_token._id);
+  if (index !== -1) {
+    io.onlineList.splice(index, 1);
+    console.log(`${socket.decoded_token.userName}断开连接`, reason);
+  }
+}
+
+module.exports = socket;
--- middlewares/handleSocket.js
+++ middlewares/handleSocket.js
@@ -2,60 +2,21 @@
  * @Author: astar
  * @Date: 2021-02-07 09:58:06
  * @LastEditors: astar
- * @LastEditTime: 2021-05-06 16:53:09
+ * @LastEditTime: 2021-05-08 17:08:40
  * @Description: 文件描述
  * @FilePath: \koa-chat\middlewares\handleSocket.js
  */
+const socketController = require('@controllers').socket;
+
 module.exports = function handleSocket (io) {
-  let onlineList = [];
-  const chatController = require('@controllers').chat;
-  const groupController = require('@controllers').group;
-
-  async function handleMessage (msg) {
-    const { _id } = this.decoded_token;
-    const chat = { senderId: _id, content: msg.content, receiverId: msg.receiverId, receiverModel: msg.isGroup ? 'groupmodel' : 'usermodel' };
-    try {
-      let res = await chatController.addChat(chat);
-      // 若发送给群
-      // 拥有这个群的所有在线用户都要收到消息
-      if (msg.isGroup) {
-        let group = await groupController.Model.findOne({ _id: msg.receiverId, members: { $in: onlineList.map(item => item.decoded_token._id) } }, { members: 1 });
-        if (group) {
-          group.members.forEach(item => {
-            let online = onlineList.find(ele => String(ele.decoded_token._id) === String(item));
-            online && online.emit('message', res);
-          });
-        }
-      } else { // 发送给个人
-        let online = onlineList.find(item => String(item.decoded_token._id) === String(msg.receiverId));
-        online && online.emit('message', res);
-      }
-    } catch (e) {
-        console.log('---------------------------', e)
-        this.emit('error', { type: 'message', msg: e });
-      };
-  }
-
-  function handleDisconnect (reason) {
-    const socket = this;
-    const index = onlineList.findIndex(item => item.decoded_token._id === socket.decoded_token._id);
-    if (index !== -1) {
-      onlineList.splice(index, 1);
-      console.log(`${socket.decoded_token.userName}断开了`, reason);
-    }
-  }
-  
+  io.onlineList = io.onlineList || [];
+  console.log('启动socket服务')
   return function (socket) {
-    const index = onlineList.findIndex(item => item.decoded_token._id === socket.decoded_token._id);
-    if (index !== -1) {
-      onlineList[index].emit('error', { type: 'logout', msg: '该用户已在别处登录' });
-      onlineList.splice(index, 1);
-    };
-    onlineList.push(socket);
-    console.log(`${socket.decoded_token.userName}连接`);
+    // 客户端用户连接
+    socketController.handleSocketConnect.call(null, io, socket);
     // 客户端发送消息
-    socket.on("message", handleMessage);
+    socket.on("message", socketController.handleReceiveMessage.bind(null, io, socket));
     // 客户端断开连接
-    socket.on("disconnect", handleDisconnect);
+    socket.on("disconnect", socketController.handleSocketDisconnect.bind(null, io, socket));
   }
 }
